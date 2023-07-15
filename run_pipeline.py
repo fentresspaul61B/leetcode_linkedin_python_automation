2b53ed066f290fbc73186b15e0192030526d91f9
fentresspaul61B <77761564+fentresspaul61B@users.noreply.github.com>
2023-07-15 10:27:58 -0700

    pipeline script

---
+++ run_pipeline.py
@@ -0,0 +1,66 @@
+"""
+Connects the extract, transform and load steps into a single pipeline.
+Runs pipeline on schedule to check for new solved problems, every 3 
+hours, and make LI post. This pipeline is automatically triggered using 
+github actions.
+"""
+
+# Run pipeline on schedule to check for new solved problems, every night,
+# And make LI post. This pipeline is automatically triggered using github
+# actions.
+
+# pylint: disable=E0611:no-name-in-module
+from pipeline.extract   import extract
+from pipeline.transform import transform
+from pipeline.load      import create_linkedin_post_through_api
+from pipeline.load      import send_data_to_bigquery
+
+
+def run_pipeline():
+    """
+    Tests pipeline:
+    Does everything except post to linkedIN.
+    """
+
+    # 1. Extract all data.
+    try:
+        leetcode_problems_solved, big_query_problems_solved = extract()
+    except Exception as error:
+        print("Extraction failed.")
+        print(error)
+        return None
+
+    # 2. Transform
+    try:
+        new_problems, linked_in_message = transform(
+            leetcode_problems_solved,
+            big_query_problems_solved
+            )
+    except Exception as error:
+        print("Transform failed.")
+        print(error)
+        return None
+
+    # 3. load data into linkedin and into big query.
+    print(linked_in_message)
+    condition_1 = len(new_problems) > 1 and len(leetcode_problems_solved) > 1
+    condition_2 = (condition_1 and len(leetcode_problems_solved) % 100 == 0)
+    if condition_1 or condition_2:
+
+        # Post message to LinkedIn.
+        # print(linked_in_message)
+        create_linkedin_post_through_api(linked_in_message)
+
+        # Update pickle file for data backup.
+        for problem in new_problems:
+            send_data_to_bigquery(problem)
+
+
+def main():
+    # run_pipeline()
+    run_pipeline()
+    # current_problems_solved = extract_leet_code_data_from_api()
+    # update_leet_code_problem_count(current_problems_solved)
+
+if __name__ == "__main__":
+    main()
